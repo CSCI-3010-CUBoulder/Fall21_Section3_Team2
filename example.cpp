72590c0b41aa0eed1cc3ce78c61a2eff2b84de8e
Maigh Kaur Jammu <60054898+mjk7817@users.noreply.github.com>
2021-09-21 13:49:19 -0600

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,17 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include "functions_to_implement.cpp"
+#include <vector>
+
+TEST_CASE ( "FACTORIALS ARE COMPUTED","[factorial]") {
+  CHECK (Factorial(0)==0);
+  REQUIRE(Factorial(0)==1);
+  REQUIRE(Factorial(1)==1);
+
+  
+}
+TEST_CASE("EVEN MASK TEST, "[evenMask]"){
+     CHECK(OddMask(1) == 1);
+     REQUIRE(OddMask(2) ==0)
+}
